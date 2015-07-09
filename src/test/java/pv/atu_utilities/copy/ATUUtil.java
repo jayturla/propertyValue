104d315debde704dddf905e0ef4c5a9d49f47748
jayturla <hectorturla@gmail.com>
2015-07-09 12:03:07 +0800

    Clean

--- src/test/java/pv/atu_utilities/copy/ATUUtil.java
+++ src/test/java/pv/atu_utilities/copy/ATUUtil.java
@@ -5,14 +5,17 @@ import pv.library.FunctionReference;
 
 
 import atu.testng.reports.ATUReports;
+import atu.testng.reports.logging.LogAs;
+import atu.testng.selenium.reports.CaptureScreen;
+import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
 
 public class ATUUtil extends FunctionReference {
 	
 	public void performATU(String steps, String inputVal, String expected, String actual, boolean screenShot, boolean status) {
-		if(status){
-			ATUReports.add(steps, inputVal, expected, actual, screenShot); //pass
-		}else {
-			ATUReports.add(steps, inputVal, expected, actual, screenShot); //fail
+		if(status) {
+			ATUReports.add(steps,inputVal,expected,actual,screenShot);
+		}else{
+			ATUReports.add(actual, LogAs.FAILED, new CaptureScreen(ScreenshotOf.DESKTOP));
 		}
 	}
 	
