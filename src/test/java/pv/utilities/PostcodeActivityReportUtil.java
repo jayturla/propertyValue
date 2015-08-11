d2e414e928585b13a9fe119fa4a59bc6da69c085
Jerick Castro <jrick.castro@gmail.com>
2015-08-11 14:34:14 +0800

    edit

--- src/test/java/pv/utilities/PostcodeActivityReportUtil.java
+++ src/test/java/pv/utilities/PostcodeActivityReportUtil.java
@@ -28,7 +28,7 @@ public class PostcodeActivityReportUtil extends FunctionReference {
 		login.loginSite();
 		
 		type(xpath(PVObjectReferenceSmoketest.slasBox), input[suburb]);
-		driver.findElement(xpath(PVObjectReferenceSmoketest.slasBox)).sendKeys(Keys.RETURN);
+		clickselectSuggestion(0,0,0,0,false);
 		
 		waitForElementPresent(xpath(PVObjectReferenceSmoketest.btnViewProfile));
 		clickbtnViewProfile(0,0,0,0,false);
@@ -366,7 +366,29 @@ public class PostcodeActivityReportUtil extends FunctionReference {
 		
 		return click;
 	}
+	
+	public boolean clickselectSuggestion(int steps, int inputVal, int expected, int actual, boolean withATU) throws Exception {
+		boolean click = false;
+		
+		try{
+			waitForElementPresent(xpath(PVObjectReferenceSmoketest.selectSuggestion));
+			click(xpath(PVObjectReferenceSmoketest.selectSuggestion));
+			click = true;
+		}catch(Exception e){
+		}
+		
+		if(withATU) {
+			if(click){
+				atu.performATU(input[steps],input[inputVal],input[expected],input[actual],true,true);//pass
+			}else {
+				atu.performATU(input[steps],input[inputVal],input[expected],input[actual],true,false);//fail
+			}
+		}
+		
+		return click;
+	}
 }
+
 
 
 
